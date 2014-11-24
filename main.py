ed14ed435aea56d57385cc2d19393d54cb4992aa
Ivan Zubieta <christmer.zubieta@yahoo.com>
2014-11-24 23:41:42 +0800

    updated main.py.. now with web crawling function

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from eve import Eve
 from eve.io.sql import SQL, ValidatorSQL
 from tables import Stores, Location, DrugsAvail
-#from flask.ext.httpauth import HTTPBasicAuth
 from flask import request, jsonify
 from flask.ext.restful import Api, Resource
+from bs4 import BeautifulSoup
+import urllib2, csv
 
-
-#auth = HTTPBasicAuth()
 app = Eve(validator=ValidatorSQL, data=SQL)
 api = Api(app)
+ctr = 1
+
+#textfile = file('drugstores.txt', 'wb')
+c = csv.writer(open("drugstores.csv", "wb"))
 
 class InsertStore(Resource):
 
@@ -30,7 +33,26 @@ class InsertDrugs(Resource):
 api.add_resource(InsertStore, '/stores', endpoint='stores')
 api.add_resource(InsertDrugs, '/stores/<string:storename>/drugs')
 
+while (ctr <= 20):
+	index_url = 'http://www.fda.gov.ph/industry-corner/drug-industry-all-registered-drugstore-manufacturer-distributor-and-trader/drugstore?start=' + str(ctr)
+
+	drugstoreFile = urllib2.urlopen(index_url)
+	drugstoreHtml = drugstoreFile.read()
+	drugstoreFile.close()
+
+	soup = BeautifulSoup(drugstoreHtml)
+	drugstoreAll = soup.find_all("a")
+	for links in soup.select('table.zebra a[href^=/industry-corner]'):
+		print (links.get_text())
+		c.writerow([links.get_text().encode('utf-8')])
+
+	ctr = ctr + 10
+print ('Writing done...\n\n\nCheck drugstores.csv')
+
 db = app.data.driver
 db.create_all()
 app.run(debug=True)
+
+
+
 
